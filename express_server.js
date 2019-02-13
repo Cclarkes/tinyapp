4f0bf92067a4fb41dc70f8da068e521c53b97cb5
CClarkes <connorclarkes@gmail.com>
2019-02-13 02:45:46 +0000

    Added form and random # function

--- express_server.js
+++ express_server.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var app = express();
 var PORT = 8080 //Our default port for this app
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+function generateRandomString() {
+    let random = Math.random().toString(36).substring(2, 8);
+    return random;
+}
 let urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"
@@ -22,6 +28,10 @@ app.get("/urls", (req, res) => {
     res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (reg, res) => {
+    res.render("urls_new")
+})
+
 app.get("/urls/:shortURL", (req, res) => {
     let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
     res.render("urls_show", templateVars);
@@ -30,6 +40,13 @@ app.get("/urls/:shortURL", (req, res) => {
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
 });
+
+app.post("/urls", (req, res) => {
+    console.log(req.body);
+    let randomNumber = generateRandomString();
+    urlDatabase[randomNumber] = (req.body.longURL);
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  });
 
 app.listen(PORT, () => {
     console.log(`Example app listing on port ${PORT}!`)
