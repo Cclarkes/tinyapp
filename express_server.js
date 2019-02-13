df1b4531f331741682a665a0e14a2327f4d19750
CClarkes <connorclarkes@gmail.com>
2019-02-13 22:41:39 +0000

    added eit link function

--- express_server.js
+++ express_server.js
@@ -32,10 +32,22 @@ app.get("/urls/new", (reg, res) => {
     res.render("urls_new")
 })
 
+app.post("/urls/:shortURL/editing", (req, res) => {
+    urlDatabase[req.params.shortURL] = (req.body.longURL);
+    console.log(urlDatabase);
+    res.redirect("/urls");
+})
+
+app.get("/urls/:shortURL/edit", (req, res) => {
+    console.log(req.params.shortURL)
+    let editVar = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+    res.render("urls_show", editVar);
+})
 app.post("/urls/:shortURL/delete", (req, res) => {
     delete urlDatabase[req.params.shortURL];  
     res.redirect("/urls");
 })
+
 
 app.get("/urls/:shortURL", (req, res) => {
     let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
