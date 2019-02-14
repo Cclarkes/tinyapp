3787fd23ccbf5e8f43fa6fd52a6f713a1eda23b6
CClarkes <connorclarkes@gmail.com>
2019-02-14 19:50:31 +0000

    All login/logout functions complete!

---
+++ d
@@ -0,0 +1,71 @@
+[33mcommit df1b4531f331741682a665a0e14a2327f4d19750[m
+Author: CClarkes <connorclarkes@gmail.com>
+Date:   Wed Feb 13 22:41:39 2019 +0000
+
+    added eit link function
+
+[33mcommit 01e91141525b2aff542a12cf8d1a99663039e587[m
+Author: CClarkes <connorclarkes@gmail.com>
+Date:   Wed Feb 13 20:52:29 2019 +0000
+
+    delete buttons added!
+
+[33mcommit 4f0bf92067a4fb41dc70f8da068e521c53b97cb5[m
+Author: CClarkes <connorclarkes@gmail.com>
+Date:   Wed Feb 13 02:45:46 2019 +0000
+
+    Added form and random # function
+
+[33mcommit ed743b4ba555367cb225510978f5178a646c2313[m
+Author: CClarkes <connorclarkes@gmail.com>
+Date:   Wed Feb 13 00:57:56 2019 +0000
+
+    formatting changes etc
+
+[33mcommit f8c047ea4ed8d5fbd65132ab4b69e953a33f6e1e[m
+Author: CClarkes <connorclarkes@gmail.com>
+Date:   Wed Feb 13 00:33:16 2019 +0000
+
+    please end this project already
+
+[33mcommit 4f1017937a5c0d2a34f820ef17454f597c2186d4[m
+Author: CClarkes <connorclarkes@gmail.com>
+Date:   Tue Feb 12 22:09:11 2019 +0000
+
+    made EJS work
+
+[33mcommit ccd41b06a0bd7839ffe357e8eaca9f69a779ede5[m
+Author: CClarkes <connorclarkes@gmail.com>
+Date:   Tue Feb 12 21:22:17 2019 +0000
+
+    sdf
+
+[33mcommit 1949a08953e28700c130a46fc720d788461191d9[m
+Author: CClarkes <connorclarkes@gmail.com>
+Date:   Tue Feb 12 20:55:05 2019 +0000
+
+    why am I committing this single innocuous step
+
+[33mcommit 45c45a68e9aa4215a0ebe3e733353a56482cb6ca[m
+Author: CClarkes <connorclarkes@gmail.com>
+Date:   Tue Feb 12 20:52:17 2019 +0000
+
+    why am I committing this single innocuous step
+
+[33mcommit 59ce2b8864df52c64bb1a1c68c57af55b5fce0e0[m
+Author: CClarkes <connorclarkes@gmail.com>
+Date:   Tue Feb 12 20:50:00 2019 +0000
+
+    Commit 1
+
+[33mcommit 9d9c23246499c8aa9ac9dfd700fa11c950adcf63[m
+Author: CClarkes <connorclarkes@gmail.com>
+Date:   Tue Feb 12 20:44:25 2019 +0000
+
+    Setup 2
+
+[33mcommit 35bc1fae226b7ce5023bb1d1c65da34b07c8f2e1[m
+Author: CClarkes <connorclarkes@gmail.com>
+Date:   Tue Feb 12 20:41:23 2019 +0000
+
+    Repo setup
--- express_server.js
+++ express_server.js
@@ -2,74 +2,92 @@ var express = require('express');
 var app = express();
 var PORT = 8080 //Our default port for this app
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 function generateRandomString() {
-    let random = Math.random().toString(36).substring(2, 8);
-    return random;
+  let random = Math.random().toString(36).substring(2, 8);
+  return random;
 }
 let urlDatabase = {
-    "b2xVn2": "http://www.lighthouselabs.ca",
-    "9sm5xK": "http://www.google.com"
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
 };
 
 app.get("/", (req,res) => {
-    res.send("Hello!");
+  res.send("Hello!");
 });
 
 app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get("/urls", (req, res) => {
-    let templateVars = {urls: urlDatabase};
-    res.render("urls_index", templateVars);
+  let templateVars = {
+    login: (req.cookies["username"]),
+    username: req.cookies["username"],
+    urls: urlDatabase};
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (reg, res) => {
-    res.render("urls_new")
+  res.render("urls_new")
 })
 
 app.post("/urls/:shortURL/editing", (req, res) => {
-    urlDatabase[req.params.shortURL] = (req.body.longURL);
-    console.log(urlDatabase);
-    res.redirect("/urls");
+  urlDatabase[req.params.shortURL] = (req.body.longURL);
+  res.redirect("/urls");
 })
 
 app.get("/urls/:shortURL/edit", (req, res) => {
-    console.log(req.params.shortURL)
-    let editVar = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-    res.render("urls_show", editVar);
+  let editVar = { 
+    username: req.cookies["username"],
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", editVar);
 })
 app.post("/urls/:shortURL/delete", (req, res) => {
-    delete urlDatabase[req.params.shortURL];  
-    res.redirect("/urls");
+  delete urlDatabase[req.params.shortURL];  
+  res.redirect("/urls");
 })
 
+app.post("/login/", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+});
 
-app.get("/urls/:shortURL", (req, res) => {
-    let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-    res.render("urls_show", templateVars);
+app.post("/logout/", (req, res) => {
+    res.clearCookie("username");
+    res.redirect("/urls");
   });
 
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { 
+    username: req.cookies["username"],
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
+  res.json(urlDatabase);
 });
 
 app.post("/urls", (req, res) => {
-    console.log(req.body);
-    let randomNumber = generateRandomString();
-    urlDatabase[randomNumber] = (req.body.longURL);
-    res.redirect("/urls/" + randomNumber);         // Respond with 'Ok' (we will replace this)
-  });
+  let randomNumber = generateRandomString();
+  urlDatabase[randomNumber] = (req.body.longURL);
+  res.redirect("/urls/" + randomNumber);         
+});
 
   app.get("/u/:shortURL", (req, res) => {
-    const longURL = urlDatabase[req.params.shortURL]
-    res.redirect(longURL);
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
   });
 
 app.listen(PORT, () => {
-    console.log(`Example app listing on port ${PORT}!`)
+  console.log(`Example app listing on port ${PORT}!`)
 });
+
