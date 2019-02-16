509401ddd5569b41308cf0b21be394777fe601dd
CClarkes <connorclarkes@gmail.com>
2019-02-16 21:26:34 +0000

    Final push! All done!

--- express_server.js
+++ express_server.js
@@ -1,12 +1,15 @@
-var express = require('express');
-var app = express();
-var PORT = 8080 //Our default port for this app
+const express = require('express');
+const app = express();
+const PORT = 8080 //Our default port for this app
 const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
+const cookieSession = require("cookie-session")
 const bcrypt = require("bcrypt");
 
+app.use(cookieSession({
+  name: 'session',
+  keys: ["user_id"],
+}));
 
-app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
@@ -14,15 +17,14 @@ function generateRandomString() {
   let random = Math.random().toString(36).substring(2, 8);
   return random;
 }
-let urlDatabase = {};
 
+let urlDatabase = {};
 const users = {
   'hfgdsd' : {
     id: 'hfgdsd',
     email: "hello@gmail.com",
     password: "hello"
   }
-  
 };
 
 app.get("/", (req,res) => {
@@ -32,15 +34,15 @@ app.get("/", (req,res) => {
 app.get("/urls", (req, res) => {
   let templateVars = { 
     urls: urlDatabase,
-    user: users[req.cookies["user_id"]]
+    user: users[req.session.user_id]
   };
   res.render("urls_index", templateVars);
 });
-
+// Above are our homepage links
 app.get("/registration", (req,res) => {
   let templateVars = { 
     urls: urlDatabase,
-    user: req.cookies["user_id"]
+    user: req.session.user_id
   };
   res.render("registration", templateVars);
 })
@@ -49,63 +51,78 @@ app.post("/registration", (req,res) => {
   let randomID = generateRandomString();
   let userlist = users;
 
-   for(var values in userlist) {
+  for(var values in userlist) {
     if(req.body["email"] === userlist[values].email) {
-    res.status(400);
-    res.send("User already registered - please log in.")
+      res.status(400);
+      res.send("User already registered - please log in.")
       }
     }
     if(req.body["user_id"] === "" || 
-  req.body["email"] === "" || 
-  req.body["password"] === "") {
-    res.status(400);
-    res.send("Please enter a valid email/password combo to continue.")
+      req.body["email"] === "" || 
+      req.body["password"] === "") {
+        res.status(400);
+        res.send("Please enter a valid email/password combo to continue.")
   }
 
   users[randomID] = {
-  id: randomID,
-  email: (req.body["email"]),
-  password: bcrypt.hashSync((req.body['password']), 10)
-    }
-  res.cookie("user_id", randomID)
+    id: randomID,
+    email: (req.body["email"]),
+    password: bcrypt.hashSync((req.body['password']), 10)
+  }
+  req.session.user_id = randomID;
   res.redirect("/urls/")
-  })
-
+})
+// Above are both requests for registration of new users
 app.get("/urls/new", (req, res) => {
   let templateVars = { 
     urls: urlDatabase,
-    user: users[req.cookies["user_id"]]
-  };urlDatabase[req.params.shortURL]
-    if(req.cookies["user_id"] === undefined) {
-      res.redirect("/login")
-    } else {
-  res.render("urls_new", templateVars)
-    }
+    user: users[req.session.user_id]
+  };
+  if(req.session.user_id === null) {
+    res.redirect("/login");
+  } else {
+    res.render("urls_new", templateVars)
+  }
 })
 
 app.post("/urls/:shortURL/editing", (req, res) => {
-  urlDatabase[req.params.shortURL].longURL = req.body.longURL
-
+  urlDatabase[req.params.shortURL].longURL = req.body.longURL;
   res.redirect("/urls");
 })
 
 app.get("/urls/:shortURL/edit", (req, res) => {
   let templateVars = { 
     urls: urlDatabase[req.params.shortURL],
-    user: users[req.cookies["user_id"]],
+    user: users[req.session.user_id],
     shortURL: req.params.shortURL,
   };
   res.render("urls_show", templateVars);
 })
+
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];  
   res.redirect("/urls");
 })
 
+app.post("/urls", (req, res) => {
+  let randomNumber = generateRandomString();
+  urlDatabase[randomNumber] = {
+    longURL: req.body.longURL,
+    userID: req.session.user_id
+    }
+  res.redirect("/urls/" + randomNumber);
+});
+
+  app.get("/u/:shortURL", (req, res) => {
+    const longURL = urlDatabase[req.params.shortURL].longURL;
+    res.redirect("http://" + longURL);
+  });
+
+//Above are all requests responsible for creation and editing of URL's
 app.get("/login", (req, res) => {
   let templateVars = { 
     urls: urlDatabase,
-    user: users[req.cookies["user_id"]]
+    user: users[req.session.user_id]
   };
   res.render("login", templateVars);
 });
@@ -116,52 +133,31 @@ app.post("/loginSubmit", (req, res) => {
   for(var key in userList) {
     if ((req.body.email) === userList[key].email &&
     bcrypt.compareSync((req.body.password), users[key].password)) {
-    elUser = userList[key].id;
+      elUser = userList[key].id;
     }
   }
   if(elUser == null) {
     res.status(400).send("Incorrect username or password entered - please try again");
   } else {
-    res.cookie("user_id", elUser);
+    req.session.user_id = elUser;
     res.redirect("/urls");
   }
 });
 
 app.post("/logout", (req, res) => {
-    res.clearCookie("user_id");
-    res.redirect("/urls");
+  req.session.user_id = null;
+  res.redirect("/urls");
   });
-
+//Requests above are responsible for logging in and out of the website
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { 
     urls: urlDatabase[req.params.shortURL],
-    user: users[req.cookies["user_id"]],
+    user: users[req.session.user_id],
     shortURL : req.params.shortURL
   };
   res.render("urls_show", templateVars);
 });
-
-app.get("/urls.json", (req, res) => {
-  let templateVars = { urls: urlDatabase[req.params.shortURL],
-    userID: req.cookies["user_id"]
-  };
-  res.json(templateVars);
-});
-
-app.post("/urls", (req, res) => {
-  let randomNumber = generateRandomString();
-  urlDatabase[randomNumber] = {
-    longURL: req.body.longURL,
-    userID: req.cookies["user_id"]
-    }
-  res.redirect("/urls/" + randomNumber);
-});
-
-  app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL].longURL;
-  res.redirect("http://" + longURL);
-  });
-
+//This request is for going directly to the actual website you're creating a link for
 app.listen(PORT, () => {
   console.log(`Example app listing on port ${PORT}!`)
 });
---
+++ readme.md
@@ -0,0 +1,22 @@
+# TinyApp Project
+
+TinyApp is a full stack web application built with Node and Express that allows users to shorten long URLs (à la bit.ly).
+
+## Final Product
+
+!["Navigation is easy, and the design is simple"]("Screenshot 1.png")
+!["Elegant, functional website that took way too long to develop"]("Screenshot 2.png)
+
+## Dependencies
+
+- Node.js
+- Express
+- EJS
+- bcrypt
+- body-parser
+- cookie-session
+- prettier (though it wasn't used)
+## Getting Started
+
+- Install all dependencies (using the `npm install` command).
+- Run the development web server using the `node express_server.js` command.
